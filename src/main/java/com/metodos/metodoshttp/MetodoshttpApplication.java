ad4cfc14a538530484eca03d8898dfac904f996b
Elprofe <al.sepulvedam@profesor.duoc.cl>
2025-04-28 10:47:04 -0400

    first commit

---
+++ src/main/java/com/metodos/metodoshttp/MetodoshttpApplication.java
@@ -0,0 +1,13 @@
+package com.metodos.metodoshttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodoshttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodoshttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metodos/metodoshttp/MetodoshttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metodos.metodoshttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodoshttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
